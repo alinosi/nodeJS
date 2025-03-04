e0c8697f91cbbb0b5426e798e33604c835fc66d7
alinosi <146401086+alinosi@users.noreply.github.com>
2025-03-04 14:58:28 +0700

    1223

---
+++ Code_Quality/jsdoc.js
@@ -0,0 +1,29 @@
+/**
+ * Get add operation of two numbers.
+ * 
+ * @param {number} numA - The first numeric operand
+ * @param {number} numB - The second numeric operand
+ * @returns {number} Sum of numA and numB
+ */
+function add(numA, numB) {
+  return numA + numB;
+}
+
+/**
+ * Data pengguna dalam sistem.
+ * @typedef {Object} User
+ * @property {string} name - Nama pengguna.
+ * @property {number} age - Umur pengguna.
+ */
+
+/** @type {User} */
+const user = {
+    name: "Budi",
+    age: 25
+  };
+
+  
+/**
+ * JSDOC memudahkan programmer untuk menulis dokumentasi lebih rapi dan terstruktur
+ * Juga mendefinisikan variabel lebih jelas pada bahasa yang loosely type (Like javascript ^0^)
+ */
